db63ed5361ae47583ef41d1a8a5755c43ab4af6c
Michael Bramblett <michaelbramblett0@gmail.com>
2019-11-17 22:44:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  remove_element_from_start_of_array = ["wow"]
+  remove_element_from_start_of_array.
 end
 
 def retrieve_element_from_index(array, index_number)
