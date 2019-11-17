52e3c75a401f143e0f3fed7ad7b6a10818a1e032
Michael Bramblett <michaelbramblett0@gmail.com>
2019-11-17 22:48:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,6 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
+  retrieve_element_from_index = ["and","am","aim"]
   retrieve_element_from_index[2]
   
 end
